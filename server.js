68e20292fd0956ca6e4dbf244eb48824a0f3b26d
Poul Rosenberg <poul@poulrosenberg.dk>
2021-10-12 11:51:58 +0200

    updated server.js

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/live-resume'));
+app.use(express.static(__dirname + '/dist/en'));
+app.use(express.static(__dirname + '/dist/da'));
+
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/live-resume/index.html'));
+res.sendFile(path.join(__dirname+'/dist/en/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
