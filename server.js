54185bb2f3368894409d6fab3bd12e09a4e0d083
Poul Rosenberg <poul@poulrosenberg.dk>
2021-10-13 09:18:38 +0200

    debugged server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 // Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
+var PORT = 8080                         // default Heroku port
 
 // Serve only the static files form the dist directory
+app.use(express.static('./dist'));
 app.use(express.static(__dirname + '/dist/en'));
 // app.use(express.static(__dirname + '/dist/da'));
 
 
-app.get('/*', function(req,res) {
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/en/'}),
+);
     
-res.sendFile(path.join(__dirname+'/dist/en/index.html'));
-});
-
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || PORT);
